e108d39465cec5ea2ca33715b6df7a38808a3c07
MrSquidward <50464859+MrSquidward@users.noreply.github.com>
2020-02-20 20:50:57 +0100

    test

--- window.py
+++ window.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import math
+
 
 def create_circle(x, y, r, canvas, tag, color='black'):
     x0 = x - r
@@ -6,4 +8,3 @@ def create_circle(x, y, r, canvas, tag, color='black'):
     x1 = x + r
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, tags=tag, fill=color)
-
