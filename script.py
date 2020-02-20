4cfe25970d9f50235b90b741578391962ebc42e2
Ig-G <igolos@op.pl>
2020-02-20 22:40:30 +0100

    Tylko na chwile

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import window as win
 import tkinter as tk
+import physics as ph
 
 master = tk.Tk()
 master.title("Gravity simulator")
@@ -9,5 +10,13 @@ draw_frame.pack(side=tk.RIGHT)
 
 canvas = tk.Canvas(draw_frame, bg='white', height=600, width=800)
 canvas.pack()
+
+time_step = 100
+object1 = ph.GravityObject([10, 10], [0, 0], 100)
+object2 = ph.GravityObject([20, 20], [0, 0], 100)
+objects_list = [object1, object2]
+while True:
+    objects_list = ph.compute_new_param(objects_list[0], objects_list[1], time_step)
+
 
 tk.mainloop()
