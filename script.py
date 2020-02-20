eb56927e40a38162dbc989d41d83cc356cc10f51
MrSquidward <50464859+MrSquidward@users.noreply.github.com>
2020-02-20 21:38:04 +0100

    tkinter basics

--- script.py
+++ script.py
@@ -1,13 +1,22 @@
 import window as win
+import physics
 import tkinter as tk
+
 
 master = tk.Tk()
 master.title("Gravity simulator")
 
 draw_frame = tk.Frame(master, height=600, width=800)
-draw_frame.pack(side=tk.RIGHT)
+draw_frame.pack(side=tk.LEFT)
 
 canvas = tk.Canvas(draw_frame, bg='white', height=600, width=800)
 canvas.pack()
+
+input_frame = win.InputFrame(master, canvas)
+
+#testy
+
+
+#koniec testow
 
 tk.mainloop()
--- window.py
+++ window.py
@@ -1,10 +1,28 @@
 import tkinter as tk
-import math
+import physics
 
 
-def create_circle(x, y, r, canvas, tag, color='black'):
+def create_circle(x, y, r, canvas, tag='none', color='black'):
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, tags=tag, fill=color)
+
+def display_gravity_objects(g_object1, g_object2, canvas):
+    create_circle(g_object1.position[0], g_object1.position[1], 20, canvas, 'object1', 'blue')
+    create_circle(g_object2.position[0], g_object2.position[1], 20, canvas, 'object2', 'blue')
+
+def clear_canvas(canvas):
+    canvas.delete("all")
+
+def update_window(master):
+    master.update()
+
+class InputFrame:
+    def __init__(self, master, can):
+        self.root = master
+        self.canvas = can
+
+        self.frame = tk.Frame(self.root, height=600, width=200)
+        self.frame.pack(side=tk.RIGHT)
